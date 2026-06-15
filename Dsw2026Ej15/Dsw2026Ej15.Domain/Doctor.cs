6cb306655f7b7ec55b4240d87d7a8710549c1a63
Bernabe <bernabeferreyra78@gmail.com>
2026-06-15 13:27:31 -0300

    Capa de dominio implementada

---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain
+{
+    public class Doctor : BaseEntity
+    {
+        private string Name { get;}
+        private string LicenseNumber { get; }
+        private bool IsActive { get; set; }
+        private Speciality Speciality { get; set; }
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Speciality.cs
@@ -0,0 +1,9 @@
+﻿namespace Dsw2026Ej15.Domain
+{
+    public class Speciality : BaseEntity
+    {
+        private string Name { get; set; }
+        private string Description { get; set; }
+
+    }
+}
