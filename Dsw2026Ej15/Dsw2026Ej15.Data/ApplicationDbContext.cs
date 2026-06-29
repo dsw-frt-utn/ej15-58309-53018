1b80ceb2592eaa6120ca15b4f5438da0e2b2d1f6
Ariana <arianavalentinasoosa@gmail.com>
2026-06-29 16:01:40 -0300

    implemento persistencia con entity framework

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dsw2026Ej15.Domain.Entities; 
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Speciality> Specialities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //Crea un archivo llamado clinica.db en la carpeta de ejecución
+        optionsBuilder.UseSqlite("Data Source=clinica.db");
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceEf.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Dsw2026Ej15.Data.Interfaces;
+using Dsw2026Ej15.Domain.Entities;
+
+public class PersistenceEf : IPersistence
+{
+    public List<Doctor> GetActiveDoctors()
+    {
+        using var context = new ApplicationDbContext();
+        return context.Doctors.Include(d => d.Speciality).ToList();
+    }
+
+    public Doctor? GetDoctorById(Guid id)
+    {
+        using var context = new ApplicationDbContext();
+        return context.Doctors.Include(d => d.Speciality).FirstOrDefault(d => d.Id == id);
+    }
+
+    public Speciality? GetSpecialityById(Guid id)
+    {
+        using var context = new ApplicationDbContext();
+        return context.Specialities.FirstOrDefault(s => s.Id == id);
+    }
+
+    public void AddDoctor(Doctor doctor)
+    {
+        using var context = new ApplicationDbContext();
+        context.Doctors.Add(doctor);
+        context.SaveChanges();
+    }
+
+    public void RemoveDoctor(Doctor doctor)
+    {
+        using var context = new ApplicationDbContext();
+        context.Doctors.Remove(doctor);
+        context.SaveChanges();
+    }
+
+    public void InitializeData()
+    {
+        using var context = new ApplicationDbContext();
+        // me asegura que la base de datos esté creada al iniciar
+        context.Database.EnsureCreated();
+    }
+}
